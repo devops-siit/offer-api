d7bd21bc58444cf8b5279a65148252b72e64411c
Petar <quadrition@gmail.com>
2022-06-05 20:13:01 +0200

    feat(setup): initialize application

---
+++ src/main/java/com/dislinkt/offersapi/OffersApiApplication.java
@@ -0,0 +1,13 @@
+package com.dislinkt.offersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dislinkt/offersapi/OffersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dislinkt.offersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
