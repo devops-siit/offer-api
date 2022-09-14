25998a2d468a43425e6b373de2dfc81f8f40dc37
Petar Cerović <quadrition@gmail.com>
2022-09-15 01:00:49 +0200

    feat: added rabbit mq on adding company

--- src/main/java/com/dislinkt/offersapi/service/company/CompanyService.java
+++ src/main/java/com/dislinkt/offersapi/service/company/CompanyService.java
@@ -6,17 +6,22 @@ import com.dislinkt.offersapi.exception.types.EntityNotFoundException;
 import com.dislinkt.offersapi.repository.CompanyRepository;
 import com.dislinkt.offersapi.web.rest.company.payload.CompanyDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@EnableBinding(Sink.class)
 public class CompanyService {
 
     @Autowired
     private CompanyRepository companyRepository;
 
-    public CompanyDTO insertCompany(CompanyDTO companyDTO) {
+    @StreamListener(target = Sink.INPUT)
+    public void insertCompany(CompanyDTO companyDTO) {
 
         Optional<Company> companyOrEmpty = companyRepository.findOneByName(companyDTO.getName());
 
@@ -33,10 +38,6 @@ public class CompanyService {
         company.setUuid(companyDTO.getUuid());
 
         companyRepository.save(company);
-
-        //companyDTO.setUuid(company.getUuid());
-
-        return companyDTO;
     }
 
     public Company findOneByUuidOrElseThrowException(String uuid) {
